966c4785db5917054b6115cec9686cc4ab390d28
Titolu <78312272+Titolu@users.noreply.github.com>
2024-05-21 12:19:54 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -12,6 +12,9 @@ variable "public_subnet" {
   default = [ "10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24" ]
 }
 
+# Fetch the List of available AZs
+data "aws_availability_zones" "available" {}
+
 variable "private_subnet" {
   type = list(string)
   description = "Private Subnet CIDR values"
